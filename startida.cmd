67cf798134610341a2afa3cebd83840c34480bd9
gjf <gjf.fixxxer@gmail.com>
2019-02-03 18:21:29 +0300

    Add files via upload

--- startida.cmd
+++ startida.cmd
@@ -8,8 +8,8 @@ REM Copyrights: © Gray Jack the Fixxxer
 REM Trademarks: 
 REM Originalname: StartIDA.exe
 REM Comments: Works with IDA 6.8
-REM Productversion:  1. 7. 0. 0
-REM Fileversion:  1. 7. 0. 0
+REM Productversion:  1. 8. 0. 0
+REM Fileversion:  1. 8. 0. 0
 REM Internalname: StartIDA.exe
 REM ExeType: ghost
 REM Architecture: x86
@@ -19,11 +19,17 @@ REM  QBFC Project Options End
 @ECHO ON
 set xOS=x64& If "%PROCESSOR_ARCHITECTURE%"=="x86" (If Not Defined PROCESSOR_ARCHITEW6432 Set xOS=x86)
 set param=%~1
+xcopy /E /I /C /Y /Q /H /R "%appdata%\zynamics" ".\Backup\zynamics"
+xcopy /E /I /C /Y /Q /H /R "%appdata%\Hex-Rays" ".\Backup\Hex-Rays"
+xcopy /E /I /C /Y /Q /H /R "%appdata%\IDA Pro" ".\Backup\IDA Pro"
+call :removedir "%appdata%\zynamics"
+call :removedir "%appdata%\Hex-Rays"
+call :removedir "%appdata%\IDA Pro"
 xcopy /E /I /C /Y /Q /H /R ".\BinDiff\INI" "%appdata%\"
 xcopy /E /I /C /Y /Q /H /R ".\Hex-Rays" "%appdata%\"
 xcopy /E /I /C /Y /Q /H /R ".\Hex-Rays\IDA Pro" "%appdata%\IDA Pro"
 reg export HKEY_CURRENT_USER\Software\Hex-Rays backup.reg /y
-reg import settings.dat
+reg import settings.reg
 if "%param%"=="32" goto x32
 if "%param%"=="64" goto x64
 if "%param:~1%"=="32" goto x32
@@ -39,6 +45,11 @@ goto end
 start /wait idaq64.exe
 goto end
 
+:removedir
+del /F /Q /S %1 > nul
+rmdir /s /q %1
+exit /b
+
 :end
 reg export HKEY_CURRENT_USER\Software\Hex-Rays settings.reg /y
 xcopy /E /I /C /Y /Q /H /R "%appdata%\zynamics\*" ".\BinDiff\INI\zynamics"
@@ -47,7 +58,9 @@ xcopy /E /I /C /Y /Q /H /R "%appdata%\IDA Pro" ".\Hex-Rays\IDA Pro"
 reg delete HKEY_CURRENT_USER\Software\Hex-Rays /f
 reg import backup.reg
 del /F /Q backup.reg
-rd /S /Q "%appdata%\zynamics"
-rd /S /Q "%appdata%\Hex-Rays"
-rd /S /Q "%appdata%\IDA Pro"
-del %~s0 /q /f
+call :removedir "%appdata%\zynamics"
+call :removedir "%appdata%\Hex-Rays"
+call :removedir "%appdata%\IDA Pro"
+xcopy /E /I /C /Y /Q /H /R ".\Backup\*" "%appdata%\"
+call :removedir Backup
+del /q /f %~s0
